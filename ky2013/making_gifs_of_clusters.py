14429144619c07e993ea3afa42b9e41b5027f026
tbeleyur <tbeleyur@orn.mpg.de>
2022-09-13 16:40:16 +0200

    Add making gifs module

---
+++ ky2013/making_gifs_of_clusters.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Making GIF from multiple png files
+===================================
+Activate  ushichkawebsite environment in Win10, and otherwise
+install moviepy and natsort. 
+
+Created on Thu Sep  8 16:59:56 2022
+
+@author: theja
+"""
+import moviepy
+from moviepy.editor import ImageSequenceClip
+import natsort
+import glob
+
+image_files = natsort.natsorted(glob.glob('*.png'))
+clip = ImageSequenceClip(image_files, fps=2.5)
+clip.write_gif('clustered_positions.gif', fps=2.5)
+clip.close()
+
+
+
