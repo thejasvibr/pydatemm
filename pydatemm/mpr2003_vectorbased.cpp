08116a87efa56d9110618ab4554228ab1accf4fb
tbeleyur <tbeleyur@orn.mpg.de>
2022-11-01 11:09:49 +0100

    Implement solve_eqn_22 partially - few more lines to go.

--- pydatemm/mpr2003_vectorbased.cpp
+++ pydatemm/mpr2003_vectorbased.cpp
@@ -11,32 +11,49 @@
 #include "eigen_utils.h"
 #include "tdoa_residual.h"
 
-	
 using Eigen::MatrixXd;
 using Eigen::ArrayXXd;
 using Eigen::ArrayXd;
-using Eigen::Vector3d;
+using Eigen::Vector3d, Eigen::Vector2d;
 using Eigen::VectorXd;
 using Eigen::ArithmeticSequence;
 using Eigen::seq;
 using namespace std;
 
 
+Vector2d  solve_eqn_22(VectorXd a, VectorXd b){
+	Vector2d Rs12;
+	
+	Rs12	<< 1,2;
+	double term1, term2, bsquare_term, term2_ii;
+	double term2_i;
+	
+	term1 = (a.array()*b.array()).sum();
+	term2_i = pow(term1, 2);
+	bsquare_term = b.array().pow(2).sum() - 1 ;
+	term2_ii = bsquare_term*(a.array().pow(2).sum());
+	term2 = sqrt(term2_i - term2_ii);
+	cout << "term2  " << term2 << endl;
+	
+	return Rs12;
+}
+
 vector<double> mpr2003_optim(const vector<double> &mic_ntde_raw, const double &c=343.0){
     int nmics = get_nmics(mic_ntde_raw);
 	cout << "num mics is " << nmics << endl;
 	VectorXd mic_ntde_vx_raw = to_VXd(mic_ntde_raw); // without any subtraction
 	VectorXd mic_ntde = to_VXd(mic_ntde_raw);
-	MatrixXd S(nmics-1,3), S_t_S(nmics-1,3);
-	MatrixXd S_t(nmics-1,3);
-	ArrayXd di(nmics-1);
-	ArrayXd z(nmics-1);
+	MatrixXd S(nmics-1,3), invS_t_S(nmics-1,3);
+	MatrixXd S_t(nmics-1,3), inv_StS_St(nmics-1,3);
+	VectorXd di(nmics-1),z(nmics-1),a(nmics-1), b(nmics-1);
+	Vector2d Rs_12;
+
     if (nmics <= 3){
         throw std::invalid_argument( "Less than or equal to 3 mics detected. Aborting." );
 		}
     int position_inds = nmics*3;  
    	VectorXd mic0 = mic_ntde.head(3);
-   	di = mic_ntde.tail(nmics-1).array();
+   	di = mic_ntde.tail(nmics-1);
 
    	MatrixXd R_inv(3, nmics-1);
    	ArithmeticSequence starts = seq(3, position_inds-3, 3);
@@ -44,43 +61,74 @@ vector<double> mpr2003_optim(const vector<double> &mic_ntde_raw, const double &c
    	for (int i=0; i<starts.size(); i++){
    		mic_ntde(seq(starts[i],stops[i])) +=  -mic0;
    		}
-	
-	cout << "di " << di << endl;
-	
+
     // 0th sensor is assumed to be at origin
 	S = mic_ntde(seq(3,position_inds-1)).reshaped(3,nmics-1).transpose();
 	ArrayXXd S_sq_rowsum(S.rows(), S.cols());
 	S_sq_rowsum = S.array().pow(2).rowwise().sum();
-	cout << "Ssq \n" << endl;
-	cout << S_sq_rowsum << endl;
     // eqn. 12
-	//cout<< pow(S,2) <<endl;
-	
-	//cout << "Ssq" << S_sq_rowsum << endl;
-    z = S_sq_rowsum - di.pow(2); // row-wise sum of S^2
+
+    z = S_sq_rowsum - di.array().pow(2); // row-wise sum of S^2
     cout << "z" << z << endl;
 	z *= 0.5;
-	cout << " z 0.5 \n " << z << endl;
-	//inv_StS
-	cout << "S \n" << S << endl;
+	//eqn. 17 - without the weighting matrix R
 	S_t = S.replicate(1,1).transpose();
 	cout << "S_t \n" << S_t << endl;
-	S_t_S.noalias() = S_t*S;
-	cout << "S_t_S inver" << S_t_S.inverse() << endl;
-    /*# eqn. 17 - without the weighting matrix R
-    inv_StS = np.linalg.inv(np.dot(S.T,S));
-    inv_StS_St = np.dot(inv_StS, S.T);
-    a = np.dot(inv_StS_St, z);
-    # eqn. 18
+	invS_t_S = (S_t*S).inverse();
+	inv_StS_St = invS_t_S*S_t;
+	cout << "inv_StS_St \n" << inv_StS_St << endl;
+	a = inv_StS_St*z;
+	// eqn. 22
+	b = inv_StS_St*di;
+    
+	//eqn. 22
+	Rs_12 = solve_eqn_22(a, b);
+ 
+    /*# eqn. 18
     b = np.dot(inv_StS_St, di);
     # eqn. 22
     Rs_12= solve_eqn_22(a, b);
     # substitute Rs into eqn. 19
-    xs = choose_correct_mpr_solutions(mic_array, Rs_12, (a,b), di);
+    //xs = choose_correct_mpr_solutions(mic_array, Rs_12, (a,b), di);
     return xs;*/
 	vector<double> out = {0,1.5,2.4};
 	return out;
 }
+
+
+
+/*
+def solve_eqn_22(a,b):
+    '''
+    Implements Equation 22 in MPR 2003. 
+
+    Parameters
+    ----------
+    a,b : (3,) np.arrays
+    
+    Returns
+    -------
+    Rs12 : (2,) np.array
+        Array holding Rs1 and Rs2 - the two range estimates arising from 
+        the quadratic equation.
+    '''
+    a1, a2, a3 = a
+    b1, b2, b3 = b
+    term1 = a1*b1 + a2*b2 + a3*b3
+    # split the numerator
+    term2_i = term1**2
+    bsquare_term = (b1**2+ b2**2 + b3**2) - 1 
+
+    term2_ii = bsquare_term*(a1**2 + a2**2 + a3**2)
+    term2 = np.sqrt(term2_i - term2_ii)
+    denominator = bsquare_term
+    numerator1 = term1 + term2
+    numerator2 = term1 - term2
+    Rs12 = np.array([numerator1/denominator, numerator2/denominator])
+    return Rs12
+
+
+*/
 
 int main(){
 	
