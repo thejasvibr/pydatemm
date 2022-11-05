a80f49907ed38c4fa2d798da3a23807b999507a2
thejasvi <thejasvib@gmai.com>
2022-11-05 09:27:26 +0100

    Complete comparison of Cpp and Python implementations. Source and residual values match to within 1e-14. This is probably the result of the different pseudo-inverse algorithms used.

--- pydatemm/miaow_2.py
+++ pydatemm/miaow_2.py
@@ -19,7 +19,7 @@ from pydatemm.source_generation import generate_candidate_sources_hybrid
 
 #%%
 kwargs['num_cores'] = 4
-kwargs['K'] = 7
+kwargs['K'] = 2
 for i in range(1):
     a = nstime()
     dcpp = generate_candidate_sources_hybrid(sim_audio, **kwargs)
@@ -28,7 +28,7 @@ for i in range(1):
 cpp_sources = np.array([each for each in dcpp.sources])
 #%%
 a = nstime()
-py_sources, py_cfls, py_tdein = generate_candidate_sources_v2(sim_audio, **kwargs)
+py_sources, py_cfls, py_tdein = generate_candidate_sources_v2.(sim_audio, **kwargs)
 b = nstime()
 print(f'Python-based: {b-a}')
 
@@ -43,11 +43,35 @@ py_nchannels = [ (len(each)+1)/4 for each in py_tdein]
 py_nch, py_counts = np.unique(py_nchannels, return_counts=True)
 
 
-#%% Check that each cpp source is also present in the numpy output
-each = cpp_sources[55222,:]
-residual = np.sqrt(np.sum((each[:3]-py_sources[:,:3])**2,1))
-print(np.argwhere(residual<1e-25))
+#%% Check that the a common input results in the same outputs from 
+# for both implementations 
+try:
+    import cppyy as cpy
+except:
+    pass
+from pydatemm.localisation import spiesberger_wahlberg_solution as sws
+from pydatemm.tdoa_quality import residual_tdoa_error_nongraph
 
+
+difference = []
+for each in py_tdein:
+    input1 = np.array(each)
+    nmics = int((len(input1)+1)/4)
+    input1_cpp = cpy.gbl.std.vector["double"](each for each in input1)
+    
+    cpp_out = cpy.gbl.sw_matrix_optim(input1_cpp)
+    cpp_out = np.array(cpp_out)
+    try:
+        py_out = sws(input1[:nmics*3].reshape(-1,3), input1[-(nmics-1):])
+        tdoa_res = residual_tdoa_error_nongraph(input1[-(nmics-1):], py_out,input1[:nmics*3].reshape(-1,3))
+        py_out = np.array([*py_out, tdoa_res])
+    except ValueError:
+        py_out = np.tile(np.nan, 4)
+
+    if not np.sum(np.isnan(py_out)) > 0:
+        difference.append(np.sqrt(np.sum((py_out-cpp_out)**2)))
+        
+difference = np.array(difference)        
 
 
 
