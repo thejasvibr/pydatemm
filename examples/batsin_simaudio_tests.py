8de0bad901fb32fbfd0f2b1bdb8398c7dc83a6b5
thejasvi <thejasvi.beleyur@bi.mpg.de>
2023-05-04 16:50:37 +0200

    Simplify the code to setup parameter files

--- examples/batsin_simaudio_tests.py
+++ examples/batsin_simaudio_tests.py
@@ -28,7 +28,7 @@ common_parameters['audiopath'] = '3-bats_trajectory_simulation_1-order-reflectio
 #common_parameters['arraygeompath'] = 'mic_xyz_multibatsim.csv'
 common_parameters['arraygeompath'] = 'mic_xyz_multibatsim_noisy0.05m.csv'
 common_parameters['dest_folder'] = 'multibatsim_results'
-common_parameters['K'] = 4
+common_parameters['K'] = 2
 common_parameters['maxloopres'] = 1e-4
 common_parameters['thresh_tdoaresidual'] = 1e-10 # s
 common_parameters['remove_lastchannel'] = "False"
@@ -69,53 +69,53 @@ common_command = "python -m pydatemm -paramfile"
 with open ('bats_simaudio_runs.sh', 'w') as rsh:  
     for paramfile in all_param_files:
         rsh.writelines(common_command + f" {paramfile}" + " & \n")
-#%%
-os.system("bash bats_simaudio_runs.sh")
-#%%
-# load all the results into a dictionary
-result_files = natsorted(glob.glob(common_parameters['dest_folder']+'/*.csv'))
-all_results = []
-if len(result_files)==relevant_time_windows.size-1:
-    results = {}
-    for i in range(relevant_time_windows.size-1):
-        all_results.append(pd.read_csv(result_files[i]))
-else: 
-    raise IndexError('Num result files dont match the time poitns')
-all_sources = pd.concat(all_results).reset_index(drop=True)
-all_posns = all_sources.loc[:,['x','y','z','tdoa_res']].to_numpy()
+# #%%
+# os.system("bash bats_simaudio_runs.sh")
+# #%%
+# # load all the results into a dictionary
+# result_files = natsorted(glob.glob(common_parameters['dest_folder']+'/*.csv'))
+# all_results = []
+# if len(result_files)==relevant_time_windows.size-1:
+#     results = {}
+#     for i in range(relevant_time_windows.size-1):
+#         all_results.append(pd.read_csv(result_files[i]))
+# else: 
+#     raise IndexError('Num result files dont match the time poitns')
+# all_sources = pd.concat(all_results).reset_index(drop=True)
+# all_posns = all_sources.loc[:,['x','y','z','tdoa_res']].to_numpy()
                                                         
-#%%
-flight_traj = pd.read_csv('multibatsim_xyz_calling.csv')
-call_positions = flight_traj[flight_traj['emission_point']]
+# #%%
+# flight_traj = pd.read_csv('multibatsim_xyz_calling.csv')
+# call_positions = flight_traj[flight_traj['emission_point']]
 
-dist_mat = distance_matrix(all_posns[:,:3], call_positions.loc[:,'x':'z'].to_numpy())
+# dist_mat = distance_matrix(all_posns[:,:3], call_positions.loc[:,'x':'z'].to_numpy())
 
-#%% Filter out those points that are within ~2  meters of the known flight
-# trajectories
-distmat_flighttraj = distance_matrix(all_posns[:,:3], flight_traj.loc[:,'x':'z'].to_numpy())
-sensible_posns = np.argwhere(distmat_flighttraj<=0.3)
-all_sensible_posns = all_posns[sensible_posns[:,0],:3]
-#%%
-try:
-    box = pv.Box(bounds=(0,4,0,9,0,3))
-    plotter = pv.Plotter()
-    plotter.add_mesh(box, opacity=0.3)
-    colors = ['r', 'b', 'k']
+# #%% Filter out those points that are within ~2  meters of the known flight
+# # trajectories
+# distmat_flighttraj = distance_matrix(all_posns[:,:3], flight_traj.loc[:,'x':'z'].to_numpy())
+# sensible_posns = np.argwhere(distmat_flighttraj<=0.3)
+# all_sensible_posns = all_posns[sensible_posns[:,0],:3]
+# #%%
+# try:
+#     box = pv.Box(bounds=(0,4,0,9,0,3))
+#     plotter = pv.Plotter()
+#     plotter.add_mesh(box, opacity=0.3)
+#     colors = ['r', 'b', 'k']
     
-    # plot the flight trajectories and call emission points
-    for key, subdf in flight_traj.groupby('batnum'):
-        for each in subdf.loc[:,'x':'z'].to_numpy():
-            plotter.add_mesh(pv.Sphere(0.05, center=each), color=colors[key-1])
-    for key, subdf in call_positions.groupby('batnum'):
-        for each in subdf.loc[:,'x':'z'].to_numpy():
-            plotter.add_mesh(pv.Sphere(0.1, center=each), color=colors[key-1])
-    # include the mic array
-    for each in array_geom:
-        plotter.add_mesh(pv.Sphere(0.03, center=each), color='g')
+#     # plot the flight trajectories and call emission points
+#     for key, subdf in flight_traj.groupby('batnum'):
+#         for each in subdf.loc[:,'x':'z'].to_numpy():
+#             plotter.add_mesh(pv.Sphere(0.05, center=each), color=colors[key-1])
+#     for key, subdf in call_positions.groupby('batnum'):
+#         for each in subdf.loc[:,'x':'z'].to_numpy():
+#             plotter.add_mesh(pv.Sphere(0.1, center=each), color=colors[key-1])
+#     # include the mic array
+#     for each in array_geom:
+#         plotter.add_mesh(pv.Sphere(0.03, center=each), color='g')
     
-    for every in all_sensible_posns:
-        plotter.add_mesh(pv.Sphere(0.2, center=every[:3]), color='white', opacity=0.5)
+#     for every in all_sensible_posns:
+#         plotter.add_mesh(pv.Sphere(0.2, center=every[:3]), color='white', opacity=0.5)
     
-    plotter.show()
-except:
-    print('Cant execute pyvista visualisation')
+#     plotter.show()
+# except:
+#     print('Cant execute pyvista visualisation')
