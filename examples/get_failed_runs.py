51f42f4c5ed12d8c7cb468fea35beb23987d0f4e
tbeleyur <tbeleyur@ab.mpg.de>
2023-05-10 12:20:12 +0200

    latest version of get_failed_runsy

--- examples/get_failed_runs.py
+++ examples/get_failed_runs.py
@@ -2,14 +2,15 @@ import argparse
 import glob
 from natsort import natsorted
 
-parser = argparse.ArgumentParser(help="Prints out all files with the word error in them. Useful to filter out job runs that \
-didnt run due to an error of some kind")
-parser.add_argument('-pattern', type=str)
+parser = argparse.ArgumentParser(description="Gets all files with the word error in them")
+parser.add_argument('--pattern', type=str)
 
 args = parser.parse_args()
 
-err_files = natsorted(glob.glob(args.pattern))
-print(err_files)
+print(f'ARG Pattern: {args.pattern} \\n ')
+
+err_files = natsorted(glob.glob(str(args.pattern)))
+
 
 # keep those files that were either cancelled or killed. 
 unsuccessful_runs = []
@@ -21,5 +22,6 @@ for each in err_files:
             unsuccessful_runs.append(each)
             break
 
-print(unsuccessful_runs)
+
+print(natsorted(unsuccessful_runs))
 
