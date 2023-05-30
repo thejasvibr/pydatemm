e6f720c16536113f57436ea728c171292424e6b7
tbeleyur <tbeleyur@orn.mpg.de>
2023-05-30 21:14:01 +0200

    Make batsin_simaudio... take command line arguments - and use this to extend the power of slurm batch scripts

--- examples/batsin_simaudio_tests.py
+++ examples/batsin_simaudio_tests.py
@@ -5,56 +5,74 @@ Here we'll run the
 
 
 '''
-import glob
-from natsort import natsorted
-import matplotlib.pyplot as plt
+import argparse
 import numpy as np 
 import pandas as pd
-try:
-    import pyvista as pv
-except:
-    print('Cant import pyvista!')
-import soundfile as sf
-from scipy.spatial import distance_matrix, distance
-euclidean = distance.euclidean
 import os
-import subprocess
-import time
-import tqdm
+import pathlib
 import yaml
 
-common_parameters = {}
-common_parameters['audiopath'] = '3-bats_trajectory_simulation_1-order-reflections.wav'
-common_parameters['arraygeompath'] = 'mic_xyz_multibatsim.csv'
-#common_parameters['arraygeompath'] = 'mic_xyz_multibatsim_noisy0.05m.csv'
-common_parameters['dest_folder'] = 'multibatsim_results_origxyz'
-common_parameters['K'] = 3
-common_parameters['maxloopres'] = 1e-4
-common_parameters['thresh_tdoaresidual'] = 1e-8 # s
-common_parameters['remove_lastchannel'] = "False"
-common_parameters['min_peak_dist'] = 0.25e-3 # s
-common_parameters['window_size'] = 0.010 # s
-common_parameters['run_name'] = 'simaudio'# the posix timestamp will be added later!
+# thanks Fransisco (https://stackoverflow.com/a/71845536/4955732)
+def namespace_to_dict(namespace):
+    return {
+        k: namespace_to_dict(v) if isinstance(v, argparse.Namespace) else v
+        for k, v in vars(namespace).items()
+    }
 
-simdata = pd.read_csv('multibatsim_xyz_calling.csv').loc[:,'x':]
-array_geom = pd.read_csv(common_parameters['arraygeompath']).loc[:,'x':'z'].to_numpy()
 
-#%% Make the yaml file for the various time points
-step_size = 0.001
-window_size = 0.010
+args = argparse.ArgumentParser()
+args.add_argument('-audiopath',type=str, help='path to audio file')
+args.add_argument('-arraygeompath', type=str, help='Path to array geometry csv file')
+args.add_argument('-dest-folder', type=str, )
+args.add_argument('-K', type=int)
+args.add_argument('-maxloopres', type=float)
+args.add_argument('-thresh-tdoaresidual', type=float)
+args.add_argument('-remove-lastchannel', type=str)
+args.add_argument('-min-peak-dist', type=float)
+args.add_argument('-window-size', type=float)
+args.add_argument('-run-name', type=str)
+args.add_argument('-step-size', type=float, default=1e-3)
+args.add_argument('-num-jobs', type=int, default=10)
+common_parameters = args.parse_args()
+
+for arg in vars(common_parameters):
+    print(arg, getattr(common_parameters, arg))
+
+
+# common_parameters = {}
+# common_parameters['audiopath'] = '3-bats_trajectory_simulation_1-order-reflections.wav'
+# common_parameters['arraygeompath'] = 'mic_xyz_multibatsim.csv'
+# #common_parameters['arraygeompath'] = 'mic_xyz_multibatsim_noisy0.05m.csv'
+# common_parameters['dest_folder'] = 'multibatsim_results_origxyz'
+# common_parameters['K'] = 3
+# common_parameters['maxloopres'] = 1e-4
+# common_parameters['thresh_tdoaresidual'] = 1e-8 # s
+# common_parameters['remove_lastchannel'] = "False"
+# common_parameters['min_peak_dist'] = 0.25e-3 # s
+# common_parameters['window_size'] = 0.010 # s
+# common_parameters['run_name'] = 'simaudio'# the posix timestamp will be added later!
+
+# simdata = pd.read_csv('multibatsim_xyz_calling.csv').loc[:,'x':]
+# array_geom = pd.read_csv(common_parameters['arraygeompath']).loc[:,'x':'z'].to_numpy()
+
+# #%% Make the yaml file for the various time points
+step_size = common_parameters.step_size
+window_size = common_parameters.window_size
 time_starts = np.arange(0, 0.5, step_size)
 
-if not os.path.exists(common_parameters['dest_folder']):
-    os.mkdir(common_parameters['dest_folder'])
+if not os.path.exists(common_parameters.dest_folder):
+    os.mkdir(common_parameters.dest_folder)
 
 # split the time_windows according to the total number of cores to be used.
-split_timepoints = np.array_split(time_starts, 10)
+split_timepoints = np.array_split(time_starts, common_parameters.num_jobs)
 #%%
+common_parameters_yaml = namespace_to_dict(common_parameters)
+
 for i, each in enumerate(split_timepoints):
-    common_parameters['start_time'] = str(each.tolist())[1:-1]
+    common_parameters_yaml['start_time'] = str(each.tolist())[1:-1]
     
-    fname = os.path.join(common_parameters['dest_folder'], 
-                         f'paramset_{common_parameters["run_name"]}_{i}.yaml')
+    fname = os.path.join(common_parameters.dest_folder, 
+                          f'paramset_{common_parameters_yaml["run_name"]}_{i}.yaml')
     ff = open(fname, 'w+')
-    yaml.dump(common_parameters, ff)
+    yaml.dump(common_parameters_yaml, ff)
 
--- examples/simaudio_runs.sh
+++ examples/simaudio_runs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time 00:10:00
-#SBATCH --mem 2G
-#SBATCH --array=0-9
+#SBATCH --cpus-per-task=4
+#SBATCH --time 00:15:00
+#SBATCH --mem 4G
+#SBATCH --array=0-4
 #SBATCH -o simaudio_%a.out
 #SBATCH -e simaudio_%a.err
 #SBATCH --job-name=simaudio
@@ -25,8 +25,23 @@ cd /u/tbeleyur/pydatemm/examples/
 # Run the simulated audio first
 python  multibatsimulation.py
 # setup the parameter files 
-python batsin_simaudio_tests.py
-# run the parameter files
-python -m pydatemm -paramfile multibatsim_results/paramset_simaudio_$SLURM_ARRAY_TASK_ID.yaml
 
+# original xyz
+python .\batsin_simaudio_tests.py -audiopath .\simaudio_input\3-bats_trajectory_simulation_1-order-reflections.wav -arraygeompath .\simaudio_input\mic_xyz_multibatsim.csv -K 3 -maxloopres 1e-4 -thresh-tdoaresidual 1e-8 -remove-lastchannel 'False' -min-peak-dist 0.25e-3 -window-size 0.01 -run-name 'origxyz' -step-size 1e-3 -num-jobs 5 -dest-folder simtests
+
+# 5 cm xyz error in mic array
+python .\batsin_simaudio_tests.py -audiopath .\simaudio_input\3-bats_trajectory_simulation_1-order-reflections.wav -arraygeompath .\simaudio_input\mic_xyz_multibatsim_noisy0.05m.csv -K 3 -maxloopres 1e-4 -thresh-tdoaresidual 1e-8 -remove-lastchannel 'False' -min-peak-dist 0.25e-3 -window-size 0.01 -run-name '5cmxyzerror' -step-size 1e-3 -num-jobs 5 -dest-folder simtests
+
+
+# 10 cm xyz error in mic array
+python .\batsin_simaudio_tests.py -audiopath .\simaudio_input\3-bats_trajectory_simulation_1-order-reflections.wav -arraygeompath .\simaudio_input\mic_xyz_multibatsim_noisy0.1m.csv -K 3 -maxloopres 1e-4 -thresh-tdoaresidual 1e-8 -remove-lastchannel 'False' -min-peak-dist 0.25e-3 -window-size 0.01 -run-name '10cmxyzerror' -step-size 1e-3 -num-jobs 5 -dest-folder simtests
+
+
+
+# run the localisation 
+python -m pydatemm -paramfile multibatsim_results/paramset_origxyz_$SLURM_ARRAY_TASK_ID.yaml
+
+python -m pydatemm -paramfile multibatsim_results/paramset_5cmxyzerror_$SLURM_ARRAY_TASK_ID.yaml
+
+python -m pydatemm -paramfile multibatsim_results/paramset_10cmxyzerror_$SLURM_ARRAY_TASK_ID.yaml
 
