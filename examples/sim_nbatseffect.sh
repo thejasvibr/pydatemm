6e26a07ad655f6731584daa3ad64cf5fd936d729
tbeleyur <tbeleyur@orn.mpg.de>
2023-06-01 16:37:34 +0200

    mindor changes

--- examples/sim_nbatseffect.sh
+++ examples/sim_nbatseffect.sh
@@ -2,8 +2,8 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time 00:20:00
-#SBATCH --mem 4G
+#SBATCH --time 00:45:00
+#SBATCH --mem 3G
 #SBATCH --array=0-4
 #SBATCH -o simaudio_%A_%a.out
 #SBATCH -e simaudio_%A_%a.err
@@ -37,14 +37,14 @@ do
 	  :
 	fi
 	# generate the 
-    python  multibatsimulation.py -nbats $numbats -ncalls 5 -all-calls-before 0.1 -room-dim '4,9,3' -seed 78464 -input-folder 'multibat_stresstests/nbat${numbats}/' 
+    python  multibatsimulation.py -nbats $numbats -ncalls 5 -all-calls-before 0.1 -room-dim '4,9,3' -seed 78464 -input-folder '${main_out}/nbat${numbats}/' 
 	# prepare parameter sets
-    python batsin_simaudio_tests.py -audiopath multibat_stresstests/nbat${numbats}/${numbats}-bats_trajectory_simulation_1-order-reflections.wav -arraygeompath multibat_stresstests/nbat${numbats}/mic_xyz_multibatsim.csv -K 3 -maxloopres 1e-4 -thresh-tdoaresidual 1e-8 -remove-lastchannel 'False' -min-peak-dist 0.25e-3 -window-size 0.01 -run-name 'nbats-${numbats}' -step-size 1e-3 -num-jobs 5 -dest-folder ${main_out}nbats${numbats}outdata
+    python batsin_simaudio_tests.py -audiopath ${main_out}/nbat${numbats}/${numbats}-bats_trajectory_simulation_1-order-reflections.wav -arraygeompath ${main_out}/nbat${numbats}/mic_xyz_multibatsim.csv -K 3 -maxloopres 1e-4 -thresh-tdoaresidual 1e-8 -remove-lastchannel 'False' -min-peak-dist 0.25e-3 -window-size 0.01 -run-name "nbats${numbats}" -step-size 1e-3 -num-jobs 5 -dest-folder ${main_out}nbats${numbats}outdata
 done
 
 # now perform the tracking
 for numbats in 2 4 8
 do 
-	python -m pydatemm -paramfile ${main_out}nbats${numbats}outdata/paramset_nbats-${numbats}_${SLURM_ARRAY_TASK_ID}.yaml
+	python -m pydatemm -paramfile ${main_out}nbats${numbats}outdata/paramset_nbats${numbats}_${SLURM_ARRAY_TASK_ID}.yaml
 done 
 
